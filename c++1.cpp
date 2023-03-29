67edefb92a92f73565998cba03b30ea18e8e05c0
Ferozcp98 <ferozahmedemoncse@gmail.com>
2023-03-30 02:27:37 +0600

    first commit

---
+++ c++1.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"My Name is Feroz Ahmed\n"<<"My Father's name is Md. Ruhul Amin\n"<<"My Mother's name is Mst. Feroza Begum\n";
+    return 0;
+}
